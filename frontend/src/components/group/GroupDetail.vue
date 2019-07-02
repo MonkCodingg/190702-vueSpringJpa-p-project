bfba8db3b500af7617d7cc4c1ec0a420d014a777
unknown <laevus.j@gmail.com>
2019-07-03 07:56:59 +0900

    07:56

--- frontend/src/components/group/GroupDetail.vue
+++ frontend/src/components/group/GroupDetail.vue
@@ -1,13 +1,37 @@
 <template>
-<div class="container">
+<div class="container-fluid">
     <Navbar></Navbar>
+    <div class="container">
     <section id="table-head-options">
         <p></p>
         <h2 class="section-heading mb-4">
             Woon Group Detail
         </h2>
-        
+        <table class="table">
+            
+            <tbody>
+                <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                </tr>
+                <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+                </tr>
+                <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+                </tr>
+            </tbody>
+        </table>    
     </section>
+    </div>
     <Footer></Footer>
   
 </div>    
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
-
+import GroupList from '@/components/group/GroupList'
+import GroupDetail from '@/components/group/GroupDetail'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {path: '/', name: 'Home', component: Home},
+    {path: '/group-list', name: 'group-list', component: GroupList},
+    {path: '/group-detail', name: 'group-detail', component: GroupDetail},
   ]
 })
